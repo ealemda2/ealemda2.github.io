9b96ed6427a8f02a3297168f66591c106a3316c5
Ertugrul Alemdar <ertalemdar@gmail.com>
2020-06-28 18:37:44 -0400

    build

---
+++ precache-manifest.af38277b57c4679db6c08e108592b40d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1dfbf40aaff477c26eabe1148d2402f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccb32c905f6b81d6b761",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3d3013004c89e01f7258",
+    "url": "/static/js/2.b54fff75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b54fff75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccb32c905f6b81d6b761",
+    "url": "/static/js/main.ae10ceac.chunk.js"
+  },
+  {
+    "revision": "675e917f907f28ae0f9d",
+    "url": "/static/js/runtime-main.27584f43.js"
+  }
+]);
